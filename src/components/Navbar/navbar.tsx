9fc35356e52dfb7ddc338878877c6fb63fd23d69
nagasritha <123246024+nagasritha@users.noreply.github.com>
2024-10-16 17:50:08 +0530

    empty dashboard created

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export const Navbar = ()=>{
     return (
         <nav className='navbar bg-dark shadow p-3 d-flex gap-2'>
-            <p><b><Link to='/'  className="text-white text-decoration-none">Expense Tracker</Link></b></p>
-            <p><b><Link to="dashboard" className="text-white text-decoration-none">Dashboard</Link></b></p>
+            <p><b><Link to='/user/home'  className="text-white text-decoration-none">Expense Tracker</Link></b></p>
+            <p><b><Link to="/user/dashboard" className="text-white text-decoration-none">Dashboard</Link></b></p>
         </nav>
     )
 }
