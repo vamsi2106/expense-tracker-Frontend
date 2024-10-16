545c95dce61af728784f1e1c31e5c294b89a217f
vamsi2106 <saivamsi1121@gmail.com>
2024-10-16 16:43:28 +0530

    login-admin-v2

--- src/components/authentication/LoginPage.tsx
+++ src/components/authentication/LoginPage.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { getCookie } from "../../utils/cookieUtils";
 import { useDispatch } from "react-redux";
-import { setUser } from "../../store/userSlice"; // Import the setUser action
+import "./auth.css";
+import microsoft from "../../assets/images/microsoft.png";
+import loginImg from "../../assets/images/logInImg.png";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
 
 export default function LoginPage() {
   const navigate = useNavigate();
   const dispatch = useDispatch(); // Hook for dispatching actions
+  const loadingBarRef = useRef<LoadingBarRef>(null); // Use LoadingBarRef type
 
   const authUrl =
     `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}/oauth2/v2.0/authorize?` +
@@ -20,6 +24,7 @@ export default function LoginPage() {
 
   const handleLogin = () => {
     console.log("Redirecting to Azure AD...");
+    loadingBarRef.current?.continuousStart(); // Start the loading bar
     window.location.href = authUrl;
   };
 
@@ -30,9 +35,36 @@ export default function LoginPage() {
   }, [navigate]);
 
   return (
-    <div>
-      <h2>Login Page</h2>
-      <button onClick={handleLogin}>Login with Azure AD</button>
+    <div className="login-body">
+      <LoadingBar color="#f11946" height={10} ref={loadingBarRef} />
+      <div className="login-container" id="container">
+        <div className="login-sign-in-container">
+          <img className="login-img-logo" src={loginImg} alt="Login" />
+        </div>
+        <div className="login-overlay-container">
+          <div className="login-overlay">
+            <div className="login-overlay-panel login-overlay-right">
+              <strong className="login-e1">Expenses Tracker</strong>
+              <h1>Welcome Back!</h1>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                onClick={handleLogin}
+                className="login-button login-ghost-1"
+                id="signUp"
+              >
+                Login With{" "}
+                <img
+                  className="login-ms-login-logo"
+                  src={microsoft}
+                  alt="microsoft-logo"
+                />{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+// src/declarations.d.ts
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
